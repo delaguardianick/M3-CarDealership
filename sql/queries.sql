3b3bf710838f697a65358135bfbb874add0ab74f
delaguardianick <delaguardianick@gmail.com>
2021-12-07 15:04:57 -0500

    finished implementing queries

--- sql/queries.sql
+++ sql/queries.sql
@@ -56,3 +56,45 @@ values (1, '1GNSKKE77DR377809', 22000.00, 1, 1);
 update vehicle
 set available = false
 where vehicleID = '_';
+
+-- Admin/AddVehicle
+-- VALIDATION: Year must be a 4 digit year between 2000 and the current year + 1 	
+-- VAlIDATION: If the type is new, mileage should be between 0 and 1000. Used vehicles must be 1000+.
+Insert into vehicle (vin, make, model, `year`, transmission, color, interiorColor, `type`, mileage, bodystyle, msrp, imageurl, featured, available, `description`)
+values ('1GNSKKE77DR377809', 'CHRYSLER', 'SEBRING', '2010', 2, 2, 3, 1, 152, 2, 21000.00, 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.kbb.com%2Fchrysler%2Fsebring%2F&psig=AOvVaw1SJXa26Pawuy3P8kPgkBAL&ust=1638981692405000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCPCjwd6Q0vQCFQAAAAAdAAAAABAD', 0, 1, "nice car");
+
+-- Admin/editvehicle
+Update vehicle set 
+make = ?, model = ?, `year` = ?, transmission = ?,
+color = ?, interiorcolor = ?, `type` = ?, mileage = ?,
+bodystyle = ?, msrp = ?, imageurl = ?, featured = ?,
+available = ?, `description` = ?
+where vin = ?;
+
+-- Admin/users
+select *
+from user;
+
+-- admin/adduser
+Insert into `user` (firstname, lastname, `role`)
+values ('Anna', 'Khan', 1);
+
+-- admin/EditUser
+update `user` set
+firstname = ?,
+lastname = ?,
+`role` = ?
+where userId = ?;
+
+-- reports/index
+select *
+from sale;
+
+-- report/sales
+select SUM(salePrice) as totalSalesPrice, COUNT(vehicleID) as VehiclesSold
+from sale;
+
+-- reports/inventory
+select make, `year`, model, SUM(MSRP) as stockValue
+from vehicle
+group by make, `year`, model
