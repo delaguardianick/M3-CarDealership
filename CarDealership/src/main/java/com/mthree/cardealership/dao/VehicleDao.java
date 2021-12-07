0d5c5287c178022f6c789cf2638d8227cb4d410c
yond5413 <87887705+yond5413@users.noreply.github.com>
2021-12-07 15:04:29 -0500

    Add files via upload

---
+++ CarDealership/src/main/java/com/mthree/cardealership/dao/VehicleDao.java
@@ -0,0 +1,23 @@
+package com.mthree.cardealership.dao;
+
+import com.mthree.cardealership.entity.Vehicle;
+import java.util.List;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author yond1
+ */
+public interface VehicleDao {
+
+    
+    List<Vehicle> getAllVehicles();
+    Vehicle getVehicleByVin(int VIN);
+    Vehicle addVehicle(Vehicle vehicle);
+    void updateVehicle(Vehicle vehicle);
+}
