77934dfe5da67dc53c0f5f8f16c5331eaf82afce
yond5413 <87887705+yond5413@users.noreply.github.com>
2021-12-08 10:54:13 -0500

    Add files via upload

--- CarDealership/src/main/java/com/mthree/cardealership/service/CarDealershipServiceLayer.java
+++ CarDealership/src/main/java/com/mthree/cardealership/service/CarDealershipServiceLayer.java
@@ -1,17 +1,11 @@
 package com.mthree.cardealership.service;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.mthree.cardealership.dao.SaleDAO;
 import com.mthree.cardealership.dao.UserDao;
 import com.mthree.cardealership.dao.VehicleDao;
-import com.mthree.cardealership.entity.Sale;
-import com.mthree.cardealership.entity.User;
-import com.mthree.cardealership.entity.Vehicle;
 
 @Service
 public class CarDealershipServiceLayer {
@@ -22,39 +16,4 @@ public class CarDealershipServiceLayer {
 	private VehicleDao vehicleDao;
 	@Autowired
 	private UserDao userDao;
-        
-        
-    public List<Vehicle> getNewCars(){
-        return vehicleDao.getNewCars();
-    }
-    
-     public List<Vehicle> getUsedCars(){
-        return vehicleDao.getUsedCars();
-    }
-	
-	public List<Vehicle> getFeaturedVehicles() {
-		List<Vehicle> featured = new ArrayList<>();
-		List<Vehicle> vehiclesFromDao = vehicleDao.getAllVehicles();
-		for(Vehicle v : vehiclesFromDao) {
-			if (v.isFeatured()) {
-				featured.add(v);
-			}
-		}
-		return featured;
-	}
-	
-	public Sale completeSale(String vehichleId) {
-		Sale sale = new Sale();
-		Vehicle v = vehicleDao.getVehicleByVin(vehichleId);
-		User u = userDao.getUserByUserId(2);
-		sale.setVehicleId(v.getVIN());
-		sale.setEmailAddress("example@email.com");
-		sale.setAddress(null);
-		sale.setZipCode("85365");
-		sale.setSalePrice(v.getMSRP());
-		sale.setPurchaseType(1);
-		sale.setUserId(u.getUserId());
-		v.setAvailable(false);
-		return saleDao.addSale(sale);
-	}
 }
